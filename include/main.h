eb7bb975b20543e05efccc251608af6a9e9286c9
ParthP22 <parthpatel0422@gmail.com>
2024-07-28 19:53:54 -0400

    Modified main.h header file. Initialized grid matrix and created a working edge_list from that as well

--- include/main.h
+++ include/main.h
@@ -1,10 +1,25 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "disjoint_set.h"
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#define WIDTH 600
-#define HEIGHT 600
+#define WIN_WIDTH 75
+#define WIN_HEIGHT 75
 #define UNIT_SIZE 25
+#define GRID_WIDTH WIN_WIDTH / UNIT_SIZE
+#define GRID_HEIGHT WIN_HEIGHT / UNIT_SIZE
+
+#if WIDTH_WIDTH % UNIT_SIZE != 0
+#error "WIN_WIDTH must be a multiple of UNIT_SIZE"
+#endif
+
+#if WIN_HEIGHT % UNIT_SIZE != 0
+#error "WIN_HEIGHT must be a multiple of UNIT_SIZE"
+#endif
+
+#endif
 
--- src/main.c
+++ src/main.c
@@ -1,43 +1,100 @@
 #include "main.h"
 
 int main(void)
-{
-    GLFWwindow* window;
-    DisjointSet* djs = djs_init(WIDTH * HEIGHT / UNIT_SIZE);
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+{   
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(WIDTH, HEIGHT, "Maze Generator", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
+
+    int (*grid)[GRID_WIDTH] = (int(*)[GRID_WIDTH])malloc(sizeof(int) * GRID_HEIGHT * GRID_WIDTH);
+
+    if(grid == NULL){
+        fprintf(stderr, "Malloc for \'grid\' failed");
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        printf("Failed to initialize GLAD\n");
-        return -1;
+
+    int (*edge_list)[2] = (int(*)[2])malloc(sizeof(int) * (2 * GRID_WIDTH * (GRID_WIDTH - 1)) * 2);
+
+    if(edge_list == NULL){
+        fprintf(stderr, "Malloc for \'edge_list\' failed");
+    }   
+
+    for(int i = 0, k = 1; i < GRID_HEIGHT; i++){
+        for(int j = 0; j < GRID_WIDTH; j++){
+            grid[i][j] = k;
+            k++; 
+        }
     }
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+    for(int i = 0, k = 0; i < GRID_HEIGHT; i++){
+        for(int j = 0; j < GRID_WIDTH; j++){
+            if(j + 1 < GRID_WIDTH){
+                edge_list[k][0] = grid[i][j];
+                edge_list[k][1] = grid[i][j+1];
+                k++;
+            }
+            if(i + 1 < GRID_HEIGHT){
+                edge_list[k][0] = grid[i][j];
+                edge_list[k][1] = grid[i+1][j];
+                k++;
+            }
+        }
     }
 
-    glfwTerminate();
+    printf("Nodes: [");
+    for(int i = 0; i < GRID_HEIGHT; i++){
+        for(int j = 0; j < GRID_WIDTH; j++){
+            printf("%d, ", grid[i][j]);
+        }
+    }
+    printf("]");
+
+    printf("\nEdges: [");
+    for(int i = 0; i < (2 * GRID_WIDTH * (GRID_WIDTH - 1)); i++){
+        printf("[%d, %d], ", edge_list[i][0], edge_list[i][1]);
+        
+    }
+    printf("]");
+
+
+
+    
+    // GLFWwindow* window;
+    // DisjointSet* djs = djs_init(GRID_WIDTH * GRID_HEIGHT);
+    // /* Initialize the library */
+    // if (!glfwInit())
+    //     return -1;
+
+    // /* Create a windowed mode window and its OpenGL context */
+    // window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Maze Generator", NULL, NULL);
+    // if (!window)
+    // {
+    //     glfwTerminate();
+    //     return -1;
+    // }
+
+    // /* Make the window's context current */
+    // glfwMakeContextCurrent(window);
+
+	// if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    // {
+    //     printf("Failed to initialize GLAD\n");
+    //     return -1;
+    // }
+
+    // /* Loop until the user closes the window */
+    // while (!glfwWindowShouldClose(window))
+    // {
+    //     /* Render here */
+    //     glClear(GL_COLOR_BUFFER_BIT);
+
+    //     /* Swap front and back buffers */
+    //     glfwSwapBuffers(window);
+
+    //     /* Poll for and process events */
+    //     glfwPollEvents();
+    // }
+
+    // glfwTerminate();
+    
     return 0;
 }

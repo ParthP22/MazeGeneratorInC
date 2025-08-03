46fd06bc671fdcfff98b5551836a6f9682a50ad3
ParthP22 <parthpatel0422@gmail.com>
2025-08-03 10:35:17 -0400

    feature: added Edge struct to edge.h containing init_node, term_node, orientation, rand_weight, selected attributes

--- include/edge.h
+++ include/edge.h
@@ -1,11 +1,21 @@
 #ifndef EDGE_H_
 #define EDGE_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef enum Orientation{
     HORIZONTAL = 0,
-    VERTICAL = 0
+    VERTICAL = 1
 } Orientation;
 
+typedef struct Edge{
+    int init_node;
+    int term_node;
+    Orientation orientation;
+    int rand_weight;
+    bool selected;
+} Edge;
 
 
 #endif

cf954725f8a5a63dc09a587eb3146caeb7cbd208
ParthP22 <parthpatel0422@gmail.com>
2025-08-03 17:01:22 -0400

    refactor: added comments and removed unnecessary code to further enhance readability and efficiency in maze_generator.c

--- src/maze_generator.c
+++ src/maze_generator.c
@@ -1,29 +1,28 @@
 #include "maze_generator.h"
 
+// Initialize MazeGenerator
 MazeGenerator* mazegenerator_init(){
-    //Grid is initialized with size n*n
+    // Malloc MazeGenerator struct
     MazeGenerator* maze_generator = (MazeGenerator*)malloc(sizeof(MazeGenerator));
 
+    // Grid is initialized with size m*n
     maze_generator->grid = (int(*)[GRID_WIDTH])malloc(sizeof(int) * GRID_HEIGHT * GRID_WIDTH);
-
     if(maze_generator->grid == NULL){
         fprintf(stderr, "Malloc for \'grid\' failed");
     }
 
-
-    //initializes a 2D matrix with 2*n*(n-1) rows (which is the number of edges in a grid) and
-    //4 columns for each row.
+    // Initializes an array with 2*n*(n-1) Edges (which is the number of edges in a grid)
     maze_generator->edges = (Edge*)malloc(sizeof(Edge) * (2 * GRID_WIDTH * (GRID_WIDTH - 1)));
-
     if(maze_generator->edges == NULL){
         fprintf(stderr, "Malloc for \'edges\' failed");
     }   
 
+    // Store refs for grid and edges
     int (*grid)[GRID_WIDTH] = maze_generator->grid;
     Edge* edges = maze_generator->edges;
 
 
-    //Each node in the grid is numbered 0 to (n*n) - 1
+    // Each node in the grid is numbered 0 to (n*n)-1
     for(int i = 0, k = 0; i < GRID_HEIGHT; i++){
         for(int j = 0; j < GRID_WIDTH; j++){
             grid[i][j] = k;
@@ -31,20 +30,19 @@ MazeGenerator* mazegenerator_init(){
         }
     }
 
-    //edges[k][2] == 0, then the line is horizontal
-    //edges[k][2] == 1, then the line is vertical
-    //k is referring to the node in the grid (numbered from 0,...,n*n)
-    //edges[k][0] is the initial node of the edge, and edges[k][1] is the terminal node
+    // Set the initial and terminal nodes of each edge, as well as 
+    // defining of the orientation of the edge (A maze will have either 
+    // horizontal or vertical edges)
     for(int i = 0, k = 0; i < GRID_HEIGHT; i++){
         for(int j = 0; j < GRID_WIDTH; j++){
-            //determines of the edge exists and if it will be drawn as a vertical line
+            // Determines if the edge exists and if it will be drawn as a vertical line
             if(j + 1 < GRID_WIDTH){
                 edges[k].init_node = grid[i][j];
                 edges[k].term_node = grid[i][j+1];
                 edges[k].orientation = VERTICAL;
                 k++;
             }
-            //determines of the edge exists and if it will be drawn as a horizontal line
+            // Determines if the edge exists and if it will be drawn as a horizontal line
             if(i + 1 < GRID_HEIGHT){
                 edges[k].init_node = grid[i][j];
                 edges[k].term_node = grid[i+1][j];
@@ -54,13 +52,10 @@ MazeGenerator* mazegenerator_init(){
         }
     }
 
+    // Initialize random number generator
     srand(time(NULL));
 
-    // Generate a random number between 0 and RAND_MAX
-    int random_number = rand();
-    printf("Random number (0 to RAND_MAX): %d\n", random_number);
-
-    // Generate a random number between 1 and 100
+    // Bounds for a random number between 1 and (2^31)-1
     int min = 1;
     int max = INT_MAX;
 
@@ -70,56 +65,51 @@ MazeGenerator* mazegenerator_init(){
         edges[i].selected = false;
     }
 
-
-
-    // printf("Nodes: [");
-    // for(int i = 0; i < GRID_HEIGHT; i++){
-    //     for(int j = 0; j < GRID_WIDTH; j++){
-    //         printf("%d, ", grid[i][j]);
-    //     }
-    // }
-    // printf("]");
-
+    // Initialize EdgeList with size 2*n*(n-1)
     maze_generator->edge_list = edgelist_init((2 * GRID_WIDTH * (GRID_WIDTH - 1)));
-
     if(maze_generator->edge_list == NULL){
         fprintf(stderr, "Malloc for \'edge_list\' failed");
     }
 
+    // Store ref for edge list
     EdgeList* edge_list = maze_generator->edge_list;
 
-    printf("\nEdges: [");
+    // Add all Edges from the edges array into the edge list
     for(int i = 0; i < (2 * GRID_WIDTH * (GRID_WIDTH - 1)); i++){
-        //printf("[%d, %d], ", edges[i][0], edges[i][1]);
         edgelist_add(edge_list, &edges[i]);
     }
-    printf("]");
 
     return maze_generator;
 }
 
+// Kruskal's algorithm to find Minimum Spanning Tree in a graph
 EdgeList* kruskals(MazeGenerator* maze_generator){
-    edgelist_sort(maze_generator->edge_list);
-    EdgeList* edge_list = maze_generator->edge_list;
-
-    // edgelist_to_string(edge_list);
-
+    // Create Disjoint Set to perform Union Find operations for Kruskal's algo
     DisjointSet* forest = djs_init(GRID_WIDTH * GRID_WIDTH);
-    int size = edgelist_size(edge_list);
-    // printf("Print: %d", size);
+    if(forest == NULL){
+        fprintf(stderr, "Malloc for \'forest\' failed");
+    }
 
+    // Initialize the edge_list for the Minimum Spanning Tree
     maze_generator->mst = edgelist_init(GRID_WIDTH - 1);
-    EdgeList* mst = maze_generator->mst;
-    // int* tmp = edgelist_get(edge_list,0);
-    // printf("Print %d, ", tmp[0]);
+    if(maze_generator->edge_list == NULL){
+        fprintf(stderr, "Malloc for \'edge_list\' failed");
+    }
 
+    // Store refs for edge list, edge list size, and minimum spanning tree
+    EdgeList* edge_list = maze_generator->edge_list;
+    int size = edgelist_size(edge_list);
+    EdgeList* mst = maze_generator->mst;
+
+    // Sort all edges in this list in ascending order
+    edgelist_sort(edge_list);    
+
+    // Perform Kruskal's Algorithm to find Minimum Spanning Tree of a graph
     for(int i = 0; i < size; i++){
         Edge* node = edgelist_get(edge_list,i);
         if(node == NULL){
-            // printf("Initial Index: %d", i);
             break;
         }
-        //printf("\nIndex: %d", i);
         int initial_node = djs_find_representative(forest, node->init_node);
         int terminal_node = djs_find_representative(forest, node->term_node);
         
@@ -135,6 +125,7 @@ EdgeList* kruskals(MazeGenerator* maze_generator){
             djs_union(forest, initial_node, terminal_node);
         }
     }
-    return mst;
 
+    // Return the Minimum Spanning Tree found by the algo above
+    return mst;
 }
